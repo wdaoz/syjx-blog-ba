07952b9d5fd19768070dbc7c8e144093e7a56f65
WDAOZHASEE <2847317085@qq.com>
2025-04-28 18:13:29 +0800

    Site updated: 2025-04-28 18:13:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107/","posts/20572/"];function toRandomPost(){
-    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
+var posts=["posts/20572/","posts/16107/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
